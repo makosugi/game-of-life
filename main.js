ac3c729bea18a9c2af6ba6599d4da4a6d1d6d3fa
makosugi <m7125m@gmail.com>
2026-03-07 00:48:42 +0900

    Change to fit screen size

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import GameOfLife from './game-of-life.js';
 const config = {
   type: Phaser.AUTO,
-  width: 495,
-  height: 495,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'phaser-example',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 495,
+    height:495 
+  },
   scene: GameOfLife,
 };
 const game = new Phaser.Game(config);
